4a33e0a8253f5c2d8793bd51ed4e5d693cf8a729
BriceKaszluk <kaszlukb@gmail.com>
2023-05-10 20:10:14 +0200

    refacto phishing-detector

--- pages/phishing-detector.tsx
+++ pages/phishing-detector.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect } from 'react';
 import { GetServerSidePropsContext } from 'next';
 import dynamic from 'next/dynamic';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
@@ -37,41 +37,42 @@ function PhishingDetector() {
   }, [hasAcceptedScope, router.query.hasAcceptedScope, userMails]);
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      sx={{ width: '100%' }}
-    >
-      {acceptedScopeStatus === 'loaded' && (hasAcceptedScope || router.query.hasAcceptedScope) ? (
-        <Box sx={{ width: '100%' }}>
-          {!userMails.length ? (
-            <div>
-              <Typography
-                variant="body1"
-                color="primary"
-                sx={{ margin: 'auto', width: 'fit-content', paddingBottom: "32px" }}
-              >
-                <CheckCircle sx={{ mr: 1, fontSize: 'inherit' }} />
-                Accès autorisé à Gmail
-              </Typography>
-              <Loader size={50} thickness={5} />
-            </div>
-          ) : <MailList />}
-
-          
-        </Box>
+<Box
+  display="flex"
+  justifyContent="center"
+  alignItems="center"
+  sx={{ width: '100%' }}
+>
+  {acceptedScopeStatus === 'loaded' && (hasAcceptedScope || router.query.hasAcceptedScope) ? (
+    <Box sx={{ width: '100%' }}>
+      {userMails.length ? (
+        <MailList />
       ) : (
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={requestAdditionalScope}
-          className="bg-button"
-        >
-          Autoriser l&apos;accès à ma boite Gmail
-        </Button>
+        <div>
+          <Typography
+            variant="body1"
+            color="primary"
+            sx={{ margin: 'auto', width: 'fit-content', paddingBottom: "32px" }}
+          >
+            <CheckCircle sx={{ mr: 1, fontSize: 'inherit' }} />
+            Accès autorisé à Gmail
+          </Typography>
+          <Loader size={50} thickness={5} />
+        </div>
       )}
     </Box>
+  ) : (
+    <Button
+      variant="contained"
+      color="primary"
+      onClick={requestAdditionalScope}
+      className="bg-button"
+    >
+      Autoriser l&apos;accès à ma boite Gmail
+    </Button>
+  )}
+</Box>
+
   );
 }
 
