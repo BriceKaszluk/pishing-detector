ae3dc58f413e983513f8ac1b7bdad93a9159580c
BriceKaszluk <kaszlukb@gmail.com>
2023-05-13 16:34:04 +0200

    feat: add google ads

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,18 @@ export default class MyDocument extends Document {
           {/* PWA primary color */}
           <meta name="theme-color" content="#000000" />
           <link rel="shortcut icon" href="/favicon.ico" />
+          {/* Google Ads Tag */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-B1T1X69P7P"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-B1T1X69P7P');
+              `,
+            }}
+          />
 
         </Head>
         <body>
